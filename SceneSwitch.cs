97ba11dcca2791ad40c72c1d9d3ebab261c089ad
jbrown7407 <54042125+jbrown7407@users.noreply.github.com>
2019-08-14 13:28:34 -0400

    Add files via upload

---
+++ SceneSwitch.cs
@@ -0,0 +1,16 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+
+{
+    void OnTriggerEnter(Collider other)
+    {
+		//SceneManager.LoadScene(2);
+		//change # to switch scenes manually
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		}
+	}
+}
